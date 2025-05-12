07f80741e47e579e7bb3a4f5691635cdcc402afb
d_flex <hiyoo95@gmail.com>
2025-05-12 22:23:16 +1000

    SCRUM-8 feature/car-detail : initial view draft of vehicle detail page completed

--- frontend/src/components/VehicleCardList.jsx
+++ frontend/src/components/VehicleCardList.jsx
@@ -1,12 +1,15 @@
 import VehicleCard from './VehicleCard';
 
-const VehicleCardList = ({vehicles}) => {
+const VehicleCardList = ({vehicles, onClickDetails}) => {
     return (
       <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4'>
         {vehicles.map((car) => (
-          <VehicleCard key={car._id} car={car} />
+          <VehicleCard
+            key={car._id}
+            car={car}
+            onClickDetails={onClickDetails}
+          />
         ))}
-        
       </div>
     );
 }
