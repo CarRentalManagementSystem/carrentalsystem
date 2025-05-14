160431ae79a0be29b8683d8488ac9f43054c1f72
d_flex <hiyoo95@gmail.com>
2025-05-14 13:07:13 +1000

    SCRUM-11 : home page error fix - RecommendedVehicles utilizes prop

--- frontend/src/components/VehicleCard.jsx
+++ frontend/src/components/VehicleCard.jsx
@@ -5,23 +5,23 @@ const VehicleCard = ({ vehicle, onClickDetails }) => {
   
   return (
     <div
-      key={vehicle.vehicleId}
+      key={vehicle?.vehicleId}
       className='overflow-hidden bg-white border border-gray-100 rounded shadow-sm'
     >
       <div className='flex items-center justify-center h-48 bg-secondary'>
-        <img src={`/images/${vehicle.manufacturer}-${vehicle.model}-${vehicle.techSpecs.type}.png`} alt='CarImage' className='object-contain' />
+        <img src={`/images/${vehicle?.manufacturer}-${vehicle?.model}-${vehicle?.techSpecs.type}.png`} alt='CarImage' className='object-contain' />
       </div>
       <div className='p-4'>
         <div className='flex items-start justify-between mb-1'>
           <div>
             <h3 className='text-lg font-semibold'>
-              {vehicle.manufacturer} {vehicle.model}
+              {vehicle?.manufacturer} {vehicle.model}
             </h3>
-            <p className='text-sm text-gray-500'>{vehicle.techSpecs.type}</p>
+            <p className='text-sm text-gray-500'>{vehicle?.techSpecs.type}</p>
           </div>
           <div className='text-right'>
             <p className='text-xl font-bold text-primary'>
-              ${vehicle.rentalPricePerDay}
+              ${vehicle?.rentalPricePerDay}
             </p>
             <p className='text-xs text-gray-500'>per day</p>
           </div>
@@ -30,16 +30,16 @@ const VehicleCard = ({ vehicle, onClickDetails }) => {
         <div className='flex justify-between mt-4 mb-4'>
           <div className='flex items-center gap-1'>
             <Settings2 className='w-4 h-4 text-gray-500' />
-            <span className='text-xs'>{vehicle.techSpecs.transmission}</span>
+            <span className='text-xs'>{vehicle?.techSpecs.transmission}</span>
           </div>
           <div className='flex items-center gap-1'>
             <Fuel className='w-4 h-4 text-gray-500' />
-            <span className='text-xs'>{vehicle.techSpecs.fuelType}</span>
+            <span className='text-xs'>{vehicle?.techSpecs.fuelType}</span>
           </div>
         </div>
         <button
           className='w-full bg-primary text-white py-2 hover:bg-[#4a2dc0] transition-colors rounded'
-          onClick={() => onClickDetails(vehicle.vehicleId)}
+          onClick={() => onClickDetails(vehicle?.vehicleId)}
         >
           View Details
         </button>
