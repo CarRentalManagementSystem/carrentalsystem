65735bf9374990dea7cebdfecb3caab77c582a84
d_flex <hiyoo95@gmail.com>
2025-05-18 16:17:37 +1000

    Empty carlist added.

--- frontend/src/components/VehicleCardList.jsx
+++ frontend/src/components/VehicleCardList.jsx
@@ -2,6 +2,17 @@ import VehicleCard from './VehicleCard';
 
 
 const VehicleCardList = ({ vehicles, setVehicles, dates }) => {
+
+  if (!vehicles || vehicles.length === 0) {
+    return (
+      <div className='py-12 text-center'>
+        <p className='text-xl text-gray-500'>
+          No vehicles found matching your criteria.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className='grid grid-cols-1 gap-8 pb-12 md:grid-cols-2 lg:grid-cols-4'>
       {vehicles?.map((vehicle) => (
